3cfb0c961e30f160fefc7883c2cb297542208b04
Power366 <172065612+Power366@users.noreply.github.com>
2025-07-22 16:20:11 +0700

    project storage is done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+
 import "./reset.css";
 import "./styles.css";
 import {today} from "./today.js";
+const projectLibrary = [];
 class Project{
 
-    constructor(){
+    constructor(name){
+        this.current = false;
+        this.id = crypto.randomUUID();
+        this.name = name;
         this.storage =[];
     }
     storeTodo(todoItem){
@@ -19,25 +24,84 @@ class todo{
         this.importance = importance;
     }
 
+    
 }
 function UIcontroller(){
+    function addNewProject(){
+
+
+    }
     function clear(){
-    accessDom.content.innerHTML ="";
-}
+        accessDom.content.innerHTML ="";
+    }
+    function clearInput(){
+        accessDom.projectInput.value=" ";
+    }
+    function showProject(projectItem){
+        for(const todoUnit of projectItem.storage){
+            let todoBlockUI = document.createElement("div");
+            todoBlockUI.classList = "todoBlock";
+            let todoTitleUI = document.createElement("p");
+            let todoDueDateUI = document.createElement("div");
+            let todoDescriptionUI = document.createElement("p");
+            let todoImportanceUI = document.createElement("div");
+            todoTitleUI.textContent = todoUnit.title;
+            todoDescriptionUI.textContent = todoUnit.description;
+            todoDueDateUI.textContent = todoUnit.dueDate;
+            todoImportanceUI.textContent = todoUnit.importance;
+            accessDom.content.appendChild(todoBlockUI);
+            todoBlockUI.appendChild(todoTitleUI);
+            todoBlockUI.appendChild(todoDescriptionUI);
+            todoBlockUI.appendChild(todoImportanceUI);
+            todoBlockUI.appendChild(todoDueDateUI);
+        }
+    }
+
     function createReferences(){
         return {
+        projectInput: document.querySelector("#projectInput"),
+        dialog: document.querySelector("dialog"),
+        closeBtn: document.querySelector("#closeBtn"),
+        confirmBtn: document.querySelector("#confirmBtn"),
+        projectList: document.querySelector(".projectList"),
         content: document.querySelector(".content"),
+        addProjBtn: document.querySelector(".add"),
         todayBtn: document.querySelector("#todayBtn"),
         next7daysBtn: document.querySelector("#next7daysBtn"),
         importantBtn: document.querySelector("#importantBtn")
         }
     }
+
     const accessDom = createReferences();
+    (function(){
+    accessDom.addProjBtn.addEventListener("click",()=>{
+        accessDom.dialog.showModal();
+       
+    })
+    accessDom.confirmBtn.addEventListener("click",(e)=>{
+        e.preventDefault();
+        accessDom.dialog.close();
+        projectLibrary.push(new Project(accessDom.projectInput.value));
+        console.log(projectLibrary)
+        clearInput()
+    })
+    accessDom.closeBtn.addEventListener("click",(e)=>{
+        e.preventDefault();
+        accessDom.dialog.close()
+        clearInput()
+    })
     accessDom.todayBtn.addEventListener("click",()=>{
         clear()
         
     })
 
+
+})()
+    showProject(todayProj);
+
 }
 const todayProj = new Project();
+const newTodo = new todo("Laundry","take everything from wardrobe","7/27/2025","true")
+todayProj.storeTodo(newTodo);
+console.log(todayProj.storage);
 UIcontroller();
